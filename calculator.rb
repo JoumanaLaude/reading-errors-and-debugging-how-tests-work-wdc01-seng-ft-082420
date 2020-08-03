77873c57761af974b527aa597a8d68b8046efd47
Joumana Laude <joumanalaude@gmail.com>
2020-08-03 07:57:56 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
 # Add your variables here
-first_number = 1
-second_number = 2
+first_number = Integer
+second_number = Integer
 sum = first_number+second_number
 difference = first_number-sec_number
 product = first_number*second_number
